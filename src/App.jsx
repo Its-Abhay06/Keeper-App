3fe8ade0682d0a7efe6f58d28bea783b5d53c809
Its-Abhay06 <abhay.mani29@gmail.com>
2025-07-23 11:42:07 +0530

    added dynamic note adding feature using map and props

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Note from "./Components/Note";
+import notes from "./notes";
+console.log(notes[0].content);
+
 function App() {
   return (
     <div>
       <Header />
       <Footer />
-      <Note />
+      {notes.map((noteItems) => (
+        <Note
+          key={noteItems.key}
+          title={noteItems.title}
+          content={noteItems.content}
+        />
+      ))}
     </div>
   );
 }
