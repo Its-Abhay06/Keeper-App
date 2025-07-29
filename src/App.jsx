d1915f3166ac05cd93d83d5bf428c0ed7ab8440f
Its-Abhay06 <abhay.mani29@gmail.com>
2025-07-30 01:04:30 +0530

    added  adding and deleting note dynamically by the user functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Note from "./Components/Note";
-import notes from "./notes";
-console.log(notes[0].content);
+import CreateArea from "../../../Downloads/React Learning/319-keeper-app-part-3-starting/src/components/CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNotes) {
+    setNotes((prevNotes) => [...prevNotes, newNotes]);
+  }
+  function deleteNote(id) {
+    setNotes(notes.filter((noteItems, index) => index !== id));
+  }
+
   return (
     <div>
       <Header />
       <Footer />
-      {notes.map((noteItems) => (
-        <Note
-          key={noteItems.key}
-          title={noteItems.title}
-          content={noteItems.content}
-        />
-      ))}
+      {notes.map((noteItems, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItems.title}
+            content={noteItems.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <CreateArea onAdd={addNote} />
     </div>
   );
 }
